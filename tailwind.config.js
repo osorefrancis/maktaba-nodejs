ac1c2f06939430f141b6c1dbd0e4d294d63bd796
Francis Osore <francisosore8@gmail.com>
2024-10-25 15:53:19 +0300

    Fleshed new and edit form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
+import forms from '@tailwindcss/forms'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./views/**/*.ejs'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [forms],
 }
 
